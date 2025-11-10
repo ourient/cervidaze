54d8f26cc4dc6995cedd2d3aae0cb7fd5315750e
ourient <ourient@pm.me>
2025-11-09 20:09:43 -0800

    split css sheets

--- public/assets/js/styleswitcher.js
+++ public/assets/js/styleswitcher.js
@@ -2,7 +2,7 @@ function changeStyle(style) {
     if (style == null)
         style = localStorage.getItem("style");
     if (style == null)
-        style = "/assets/css/main";
+        style = "/assets/css/light";
     document.querySelector("link[type='text/css']").setAttribute("href", style + ".css");
     localStorage.setItem("style", style);
 }
