39436b617d5fa0d0d8c78560edd8c517cbd329b8
ourient <ourient@pm.me>
2025-11-09 22:30:38 -0800

    Revert "add event listener to styleswitcher"
    This reverts commit 096c35a0dcf737775e627fad2033417b6ea6b261.

--- public/assets/js/styleswitcher.js
+++ public/assets/js/styleswitcher.js
@@ -8,18 +8,4 @@ document.addEventListener('DOMContentLoaded', () => {
         localStorage.setItem("style", style);
     }
     changeStyle();
-});
-
-
-//PREV CODE
-
-//function changeStyle(style) {
-//    if (style == null)
-//        style = localStorage.getItem("style");
-//    if (style == null)
-//        style = "/assets/css/light";
-//    document.querySelector("link[type='text/css']").setAttribute("href", style + ".css");
-//    localStorage.setItem("style", style);
-//}
-
-//changeStyle();
+})
