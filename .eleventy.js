ff6b3f5d6ba2abc3c95449c1b0840335929f6456
ourient <ourient@pm.me>
2025-11-10 13:14:20 -0800

    add cachebusting to css and js

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,9 @@ const { DateTime } = require('luxon');
 // Tells Eleventy to look for the RSS plugin
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 
+// Tells Eleventy to look for the auto cachebuster plugin
+const eleventyAutoCacheBuster = require("eleventy-auto-cache-buster")
+
 module.exports = function (eleventyConfig) {
   // This will stop the default behaviour of foo.html being turned into foo/index.html
   eleventyConfig.addGlobalData("permalink", "{{ page.filePathStem }}.html");
@@ -20,6 +23,9 @@ module.exports = function (eleventyConfig) {
 
   // Load the RSS plugin
   eleventyConfig.addPlugin(pluginRss);
+
+  // Load the auto cachebuster plugin
+  eleventyConfig.addPlugin(eleventyAutoCacheBuster);
 
   // Add the filter "topDate" to simplify the way blog dates are presented at the top of blog posts
   eleventyConfig.addFilter('topDate', (dateObj) => {
