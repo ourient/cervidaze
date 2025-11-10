096c35a0dcf737775e627fad2033417b6ea6b261
ourient <ourient@pm.me>
2025-11-09 21:09:13 -0800

    add event listener to styleswitcher

--- src/assets/js/styleswitcher.js
+++ src/assets/js/styleswitcher.js
@@ -1,10 +1,25 @@
-function changeStyle(style) {
-    if (style == null)
-        style = localStorage.getItem("style");
-    if (style == null)
-        style = "/assets/css/light";
-    document.querySelector("link[type='text/css']").setAttribute("href", style + ".css");
-    localStorage.setItem("style", style);
-}
+document.addEventListener('DOMContentLoaded', () => {
+    function changeStyle(style) {
+        if (style == null)
+            style = localStorage.getItem("style");
+        if (style == null)
+            style = "/assets/css/light";
+        document.querySelector("link[type='text/css']").setAttribute("href", style + ".css");
+        localStorage.setItem("style", style);
+    }
+    changeStyle();
+});
 
-changeStyle();
+
+//PREV CODE
+
+//function changeStyle(style) {
+//    if (style == null)
+//        style = localStorage.getItem("style");
+//    if (style == null)
+//        style = "/assets/css/light";
+//    document.querySelector("link[type='text/css']").setAttribute("href", style + ".css");
+//    localStorage.setItem("style", style);
+//}
+
+//changeStyle();
